93c0d7807b0dd0b5a8c7ea1e184f6e1653daa191
mcmadbat <mcmadbat@gmail.com>
2017-01-21 19:04:03 -0500

    update for waterloo works

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
 	res.render ('index');
 });
+
+// redirect to last stats google play listing
+router.get('/laststats', function(req, res){
+	res.redirect('https://play.google.com/store/apps/details?id=me.mcmadbat.laststats');
+});
+
+// redirect to redditatwork
+router.get('/sfwreddit', function(req, res){
+	res.redirect('https://github.com/mcmadbat/RedditAtWork');
+});
+
 
 module.exports = router;
