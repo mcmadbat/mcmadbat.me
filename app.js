b6b3087b8018adc72a3f4d80e5ab2b8bc4f323f1
David Zhang <mcmadbat@gmail.com>
2019-03-18 09:30:42 -0400

    link for batter up

--- app.js
+++ app.js
@@ -1,58 +1,57 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
-var app = express();
+var app = express()
 
 // handlebar
-var exphbs  = require('express-handlebars');
-var hbs = exphbs.create ( { 
-	defaultLayout: path.join(__dirname, '/views/layouts/main')
-});
+var exphbs = require('express-handlebars')
+var hbs = exphbs.create({
+  defaultLayout: path.join(__dirname, '/views/layouts/main')
+})
 
-
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', hbs.engine );
-app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'))
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 // routers
-var index = require('./routes/index');
+var index = require('./routes/index')
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index);
+app.use('/', index)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var msg = `CODE 404: ${req.path} does not exist!`;
-	var err = new Error( msg );
-  	err.status = 404;
-  	next(err);
-});
+app.use(function (req, res, next) {
+  var msg = `CODE 404: ${req.path} does not exist!`
+  var err = new Error(msg)
+  	err.status = 404
+  	next(err)
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-	// only leak msg in development
-	var msg = app.get('env') === 'development' ? err.message : '';
+  // only leak msg in development
+  var msg = app.get('env') === 'development' ? err.message : ''
 
-	// render the error page
-	res.status( err.status || 500 );
-	res.render ( 'error' , { layout: false, message: msg } );
-});
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error', { layout: false, message: msg })
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express')
+let router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res) {
-	res.render ('index');
-});
+router.get('/', function (req, res) {
+  res.render('index')
+})
 
 // redirect to last stats google play listing
-router.get('/laststats', function(req, res){
-	res.redirect('https://play.google.com/store/apps/details?id=me.mcmadbat.laststats');
-});
+router.get('/laststats', function (req, res) {
+  res.redirect('https://play.google.com/store/apps/details?id=me.mcmadbat.laststats')
+})
 
 // redirect to redditatwork
-router.get('/sfwreddit', function(req, res){
-	res.redirect('https://github.com/mcmadbat/RedditAtWork');
-});
+router.get('/sfwreddit', function (req, res) {
+  res.redirect('https://github.com/mcmadbat/RedditAtWork')
+})
 
+// redirect to batter up
+router.get('/apps/batterup/', function (req, res) {
+  res.redirect('https://chrome.google.com/webstore/detail/batter-up/jnafaeneoijiefchaegfgoiehafbdfob?hl=en')
+})
 
-module.exports = router;
+module.exports = router
